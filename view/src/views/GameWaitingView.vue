9fb0e186c1190233be4d98cc8cc0cbb8a438f2a1
10145349 <donghyeon.kim@kt.com>
2023-01-20 13:28:52 +0900

    대기실 화면 일부 수정

--- view/src/views/GameWaitingView.vue
+++ view/src/views/GameWaitingView.vue
@@ -1,42 +1,52 @@
 <template>
   <div class="common-layout">
-    <el-container class="layout-container-demo">
-      <el-aside class="aside" width="200px">
-
+    <el-container class="layout-container">
+      <el-aside class="aside">
           <el-menu class="aside-root-menu" :default-openeds="['1', '2']">
-            <el-sub-menu class="aside-sub-menu-1" index="1">
-              <template #title>
-              <div class="aside-sub-menu-1-title">
-                <b>개설된 방 목록</b>
-              </div>
-              </template>
-              <div v-for="room in rooms" :key="room.gameId" class="room-element">
-                <div class="room-name">{{room.roomName}}</div>
-                <el-button class="button">게임 참가</el-button>
-              </div>
-            </el-sub-menu>
+              <el-sub-menu class="aside-sub-menu-1" index="1">
+                <template #title>
+                <div class="aside-sub-menu-1-title">
+                  <b>개설된 방 목록</b>
+                </div>
+                </template>
+                <div class="room-element-wrapper">
+                  <div v-for="room in rooms" :key="room.gameId" class="room-element">
+                    <div class="room-name">{{room.roomName}}</div>
+                    <el-button class="button">게임 참가</el-button>
+                  </div>
+                </div>
+              </el-sub-menu>
             <el-sub-menu class="aside-sub-menu-2" index="2">
               <template #title>
                 <div class="aside-sub-menu-2-title">
                   <b>접속자 목록</b>
                 </div>
               </template>
+              <div class="user-element-wrapper">
+                <div v-for="user in ['김동현', '이상은']" :key="user" class="user-element">
+                  <div class="user-name">{{user}}</div>
+                  <el-button class="button">초대</el-button>
+                </div>
+              </div>
             </el-sub-menu>
           </el-menu>
-
         <div class="aside-sub-menu-3">
             <div class="aside-sub-menu-3-title">
               <div v-if="drawer == false">
+                <div class="room-create-title">
                 <b>방 만들기</b>
+                </div>
+                <div class="room-create-button">
+                  <el-button type="primary" style="margin-left: 16px" @click="drawer = true">
+                    PUSH
+                  </el-button>
+                </div>
               </div>
               <div v-if="drawer == true">
-                <b>방 닫기</b>
+                <b>방 생성 완료</b>
               </div>
             </div>
-          <el-button type="primary" style="margin-left: 16px" @click="drawer = true">
-            PUSH
-          </el-button>
-          <el-drawer v-model="drawer" title="game-play" :with-header="false" size="84%">
+          <el-drawer class="game-view-drawer" v-model="drawer" title="game-play" :with-header="false" size=1580>
             <GameView></GameView>
           </el-drawer>
         </div>
@@ -60,15 +70,12 @@
 </template>
 
 <script>
-import GameViewTest from "@/components/GameViewTest.vue";
 import GameView from "@/views/GameView.vue";
-import {ref} from 'vue';
 
 export default {
   name: "GameWaitingView",
 
   components: {
-    GameViewTest,
     GameView,
   },
 
@@ -109,10 +116,10 @@ export default {
 </script>
 
 <style scoped>
-.common-layout .layout-container-demo {
-  height: 900px;
+.common-layout .layout-container {
+  height: 960px;
 }
-.layout-container-demo .header {
+.layout-container .header {
   position: relative;
   background-color: cornsilk;
   height: 80px;
@@ -120,14 +127,15 @@ export default {
   align-items: center;
   flex-direction: row;
   justify-content: center;
+  padding: 0px;
 }
-.layout-container-demo .main {
+.layout-container .main {
   position: relative;
   background-color: blanchedalmond;
-  height: 740px;
-  padding: 0;
+  height: 800px;
+  padding: 0px;
 }
-.layout-container-demo .footer {
+.layout-container .footer {
   position: relative;
   background-color: antiquewhite;
   height: 80px;
@@ -135,38 +143,50 @@ export default {
   align-items: center;
   flex-direction: row;
   justify-content: center;
+  padding: 0px;
 }
-.layout-container-demo .aside {
+.layout-container .aside {
   width: 200px;
-  height: 900px;
+  height: 960px;
 }
 .aside-root-menu .room-element {
   background-color: darkkhaki;
   opacity: 90%;
 }
+.aside-root-menu .user-element {
+  background-color: honeydew;
+  opacity: 90%;
+}
 .aside-sub-menu-1 {
-  height: 350px;
+  height: 420px;
   width: 200px;
 }
 .aside-sub-menu-2 {
-  height: 350px;
+  height: 420px;
   width: 200px;
 }
 .aside-sub-menu-3 {
-  height: 100px;
+  height: 120px;
   width: 200px;
 }
 .box-card-1 {
-  height: 370px;
+  height: 400px;
+  border: 0px;
 }
 .box-card-2 {
-  height: 370px;
+  height: 400px;
+  border: 0px;
 }
 .room-element {
   scroll-behavior: auto;
 }
 .room-name {
   border: cadetblue;
+  font-weight: bolder;
+  margin: 5px 0 5px 0;
+}
+.user-name {
+  border-color: cadetblue;
   font-weight: bolder;
   margin: 5px 0 5px 0;
 }
@@ -182,5 +202,16 @@ export default {
 }
 .footer-title {
   font-weight: bolder;
+}
+.room-element-wrapper {
+  height: 350px;
+  overflow: auto;
+}
+.user-element-wrapper {
+  height: 350px;
+  overflow: auto;
+}
+.room-create-title {
+  margin-bottom: 10px;
 }
 </style>
